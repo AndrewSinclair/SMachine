c8aa578fe64cf29cbf1178ae8f54bec863b5d068
Andrew <andrew.george.sinclair@gmail.com>
2016-11-18 17:00:03 -0600

    Initial commit

---
+++ Machine.vb
@@ -0,0 +1,102 @@
+﻿Public Class Machine
+    Public Enum State
+        A
+        B
+        C
+    End Enum
+
+    Public Enum Trigger
+        Do1
+        Do2
+        Do3
+        Do4
+        Do5
+    End Enum
+
+    Private ReadOnly _graph As Dictionary(Of GraphKey, StateAction)
+    Private _currentState As State
+    Private _currentTrigger As Trigger
+    Private ReadOnly _endState As State
+    Private _defaultAction As Action
+
+    Public Sub New(ByVal initialState As State, ByVal finalState As State)
+        _graph = New Dictionary(Of GraphKey, StateAction)
+        _currentState = initialState
+        _endState = finalState
+    End Sub
+
+    Public Sub SetDefaultAction(ByVal defaultAction As Action)
+        _defaultAction = defaultAction
+    End Sub
+
+    Public Function GetCurrentState() As State
+        Return _currentState
+    End Function
+
+    Private Sub InvokeTrigger(ByVal trigger As Trigger)
+        Dim key As New GraphKey(_currentState, trigger)
+        Dim val As StateAction = _graph(key)
+
+        _currentState = val.GetState
+
+        val.Invoke()
+
+        If _defaultAction IsNot Nothing Then _defaultAction.Invoke()
+    End Sub
+
+    Public Sub DoTrigger(ByVal trigger As Trigger)
+        _currentTrigger = trigger
+    End Sub
+
+    Public Sub Start()
+        While _currentState <> _endState
+            InvokeTrigger(_currentTrigger)
+        End While
+    End Sub
+
+    Public Sub AddTransition(ByVal fromState As State, ByVal toState As State, ByVal trigger As Trigger, ByVal action As Action)
+        Dim key As New GraphKey(fromState, trigger)
+        Dim val As New StateAction(toState, action)
+        _graph.Add(key, val)
+    End Sub
+
+    Private Class StateAction
+        Private ReadOnly _action As Action
+        Private ReadOnly _state As State
+
+        Public Sub New(ByVal state As State, ByVal action As Action)
+            _state = state
+            _action = action
+        End Sub
+
+        Public Function GetState() As State
+            Return _state
+        End Function
+
+        Public Sub Invoke()
+            _action.Invoke()
+        End Sub
+    End Class
+
+    Private Class GraphKey
+        Private ReadOnly _fromState As State
+        Private ReadOnly _trigger As Trigger
+
+        Public Sub New(ByVal fromState As State, ByVal trigger As Trigger)
+            _fromState = fromState
+            _trigger = trigger
+        End Sub
+
+        Public Overrides Function GetHashCode() As Integer
+            Return (_fromState.ToString & _trigger.ToString).GetHashCode
+        End Function
+
+        Public Overrides Function Equals(ByVal obj As Object) As Boolean
+            If obj Is Nothing OrElse Me.GetType IsNot obj.GetType Then Return False
+
+            Dim other As GraphKey = DirectCast(obj, GraphKey)
+
+            Return _fromState.Equals(other._fromState) AndAlso _trigger.Equals(other._trigger)
+        End Function
+    End Class
+End Class
---
+++ Module1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.IO
+
+Module Module1
+
+    ReadOnly Machine As New Machine(Machine.State.A, Machine.State.C)
+    Dim _stateChoices As Dictionary(Of Machine.State, List(Of String))
+
+    Sub Main()
+        Const fileName As String = "MyNachine.txt"
+
+        Machine.SetDefaultAction(AddressOf DefaultAction)
+
+        Machine.AddTransition(Machine.State.A, Machine.State.B, Machine.Trigger.Do1, AddressOf State1Action)
+        Machine.AddTransition(Machine.State.B, Machine.State.C, Machine.Trigger.Do2, AddressOf State2Action)
+        Machine.AddTransition(Machine.State.C, Machine.State.A, Machine.Trigger.Do3, AddressOf State3Action)
+
+        _stateChoices = LoadStateChoices(fileName)
+
+        Machine.Start()
+
+        Console.WriteLine("Thanks for playing!")
+    End Sub
+
+    Private Function LoadStateChoices(ByVal fileName As String) As Dictionary(Of Machine.State, List(Of String))
+        Dim stateChoices As New Dictionary(Of Machine.State, List(Of String))
+
+        Dim lines As List(Of String) = File.ReadLines(fileName)
+
+        If lines IsNot Nothing AndAlso lines.Count > 0 AndAlso Not (lines.Count = 0 AndAlso String.IsNullOrEmpty(lines(0))) Then
+            For Each line As String In lines
+                If Not String.IsNullOrWhiteSpace(line) Then
+                    'state A
+                    '
+                    ' eat ice cream -> B
+                    ' eat cake -> C
+                End If
+            Next
+        End If
+
+        Return stateChoices
+    End Function
+
+    Sub DefaultAction()
+        Dim currState As Machine.State = Machine.GetCurrentState()
+        Dim choices As List(Of String) = _stateChoices(currState)
+        Dim trigger As Machine.Trigger = PromptChoices(choices)
+        Machine.DoTrigger(trigger)
+    End Sub
+
+    Sub State1Action()
+        Console.WriteLine("This is the beginning")
+    End Sub
+
+    Sub State2Action()
+        Console.WriteLine("This is the middle")
+    End Sub
+
+    Sub State3Action()
+        Console.WriteLine("This is the end")
+    End Sub
+
+    Function PromptChoices(ByVal choices As List(Of String)) As Machine.Trigger
+        Console.WriteLine("Choose one:")
+
+        Dim choicePrompts As List(Of String) = choices.Where(Function(c, i) i Mod 2 <> 0).ToList
+        Dim triggerNames As List(Of String) = choices.Where(Function(c, i) i Mod 2 = 0).ToList
+
+        Dim count As Integer = 0
+        choicePrompts.ForEach(Sub(prompt)
+                                  count = count + 1
+                                  Console.WriteLine("{0}. {1}", count, prompt)
+                              End Sub)
+
+        Dim val As String = Console.ReadLine()
+
+        While val Is Nothing
+            Console.WriteLine("Please choose again:")
+            choices.ForEach(Sub(selection) Console.WriteLine(selection))
+            val = Console.ReadLine()
+        End While
+
+        Dim triggerName As String = triggerNames(Integer.Parse(val) - 1)
+
+        Return [Enum].Parse(GetType(Machine.Trigger), triggerName)
+    End Function
+End Module
